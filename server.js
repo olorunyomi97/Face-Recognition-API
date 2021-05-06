ee9748e6bf6bbb467794748265654414cc696bd0
olorunyomi97 <olorunyomi97@gmail.com>
2021-05-06 20:25:21 +0100

    Port change

--- server.js
+++ server.js
@@ -61,8 +61,8 @@ app.put('/image', (req,res) => {
 })
 
 
-app.listen(3000, ()=> {
-    console.log('App running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log(`App running on port {process.env.PORT}`);
 })
 
 
